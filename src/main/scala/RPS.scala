2f74aba312d1069194b3dfd5e05cb2182aedd31d
davidhuttonlewis <d.huttonlewis@gmail.com>
2018-01-26 16:05:01 +0000

    used .toLowerCase to make code more read able

--- src/main/scala/RPS.scala
+++ src/main/scala/RPS.scala
@@ -3,14 +3,15 @@ object RPS {
   def main (args:Array[String]): Unit ={
     playerMove()
   }
-  def pickWinner(move1:String, move2:String)=(move1,move2) match {
-    case (a,b)if a.equalsIgnoreCase("rock") && b.equalsIgnoreCase("scissors") || a.equalsIgnoreCase("paper") && b.equalsIgnoreCase("rock") || a.equalsIgnoreCase("scissors") && b.equalsIgnoreCase("paper")=> "Player one wins"
-    case (a,b)if b.equalsIgnoreCase("rock") && a.equalsIgnoreCase("scissors") || b.equalsIgnoreCase("paper") && a.equalsIgnoreCase("rock") || b.equalsIgnoreCase("scissors") && a.equalsIgnoreCase("paper")=> "Player Two wins"
-    case(a,_)if a.equalsIgnoreCase("exit") => "That's it you run away!"
+  //Takes in the two choices and  decides the winner
+  def pickWinner(move1:String, move2:String)=(move1.toLowerCase,move2.toLowerCase) match {
+    case (move1,move2)if move1.equals("rock") && move2.equals("scissors") || move1.equals("paper") && move2.equals("rock") || move1.equals("scissors") && move2.equals("paper")=> "Player one wins"
+    case (move1,move2)if move2.equals("rock") && move1.equals("scissors") || move2.equals("paper") && move1.equals("rock") || move2.equals("scissors") && move1.equals("paper")=> "Player Two wins"
+    case(move1,_)if move1.equals("exit") => "That's it you run away!"
     case (_,_) => "Tie"
   }
+  //Used to decide if the AI will have a bias to a certain move or not
   def aiBias(): Boolean ={
-
     val value = scala.util.Random
     if ((randomNumber+2) == 3) return true
     return false
@@ -19,36 +20,40 @@ object RPS {
     val value = scala.util.Random
     value.nextInt(2)
   }
+  //Used with the AI as it uses numbers to pick a move randomly
   def moves(input:Int) = (input)match{
     case(a) if a == 0 => "paper"
     case(a) if a == 1 => "scissors"
     case(a) if a == 2 => "rock"
-
-
   }
+  //Use to have AI pick a move if the AI has a bias it will pick a move based on how many times the player has chosen a move
   def aiMove(rockCount:Int, paperCount:Int, scissorsCount:Int, bias:Boolean) = (rockCount, paperCount, scissorsCount, bias) match{
     case(_,_,_,false)=> moves(randomNumber())
-    case(a,b,c,true) if a > b && a > c => moves(0)
-    case(a,b,c,true) if  b > a && b > c => moves(1)
-    case(a,b,c,true) if c > a && c > b => moves(2)
+    case(rockCount,paperCount,scissorsCount,true) if rockCount > paperCount && rockCount > scissorsCount => moves(0)
+    case(rockCount,paperCount,scissorsCount,true) if  paperCount > rockCount && paperCount > scissorsCount => moves(1)
+    case(rockCount,paperCount,scissorsCount,true) if scissorsCount > rockCount && scissorsCount > paperCount => moves(2)
     case(_,_,_,true) => moves(randomNumber())
   }
+  //
   def playerMove (): Unit ={
-
     var rockCount = 0
     var paperCount = 0
     var scissorCount =0
     var running = true
     while (running) {
-      var input = readInput
+      var input = readInput.toLowerCase
+
       println(pickWinner(input, aiMove(rockCount, paperCount, scissorCount,aiBias())))
-      if (input.equalsIgnoreCase("rock")) rockCount += 1
-      if (input.equalsIgnoreCase("paper")) paperCount += 1
-      if (input.equalsIgnoreCase("scissors")) scissorCount += 1
-      if (input.equalsIgnoreCase("exit")) running = false
+      if (input.equals("rock")) rockCount += 1
+      if (input.equals("paper")) paperCount += 1
+      if (input.equals("scissors")) scissorCount += 1
+      if (input.equals("exit")) running = false
       println(rockCount+" "+paperCount+" "+scissorCount)
     }
   }
+
+  def game(){}
+
 
   def readInput: String ={
     scala.io.StdIn.readLine()
